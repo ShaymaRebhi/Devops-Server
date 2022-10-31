b037bc1cb4154711e23dbe8cc190e3cc2c170b33
Sofiene mokaddem <73641797+sofieneMoka@users.noreply.github.com>
2022-10-31 17:42:52 +0100

    update pom.xml + test operateur

--- src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/OperateurServiceImplTest.java
@@ -1,13 +1,16 @@
 package com.esprit.examen.services;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.esprit.examen.entities.Operateur;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootTest
 public class OperateurServiceImplTest {
@@ -15,23 +18,82 @@ public class OperateurServiceImplTest {
 	@Autowired
 	private OperateurServiceImpl operateurServiceImlp;
 	
+    final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+	
 	
 	@Test
-	public void TestOperateur(){
+	public void TestAddOperateur(){
 		
-		try {/*
+		try {
             Operateur operateur =new Operateur();
             operateur.setNom("taboubi");
             operateur.setPassword("123456");
             operateur.setPrenom("Sofiene");
             operateurServiceImlp.addOperateur(operateur);
-            */
-            //operateurServiceImlp.retrieveAllOperateurs();
+            LOGGER.info("Operateur added : " + operateur.getNom());
+
+        }catch (Exception e){
+
+
+            System.out.println(e.getMessage());
+        }
+		
+	}
+	
+	@Test
+	public void TestUpdateOperateur(){
+		
+		try {
             
             Operateur operateur = operateurServiceImlp.retrieveOperateur((long) 1);
-            operateur.setNom("Tijeni");
+            operateur.setNom("Mokaddem");
             operateurServiceImlp.updateOperateur(operateur);
-            //operateurServiceImlp.deleteOperateur(operateur.getIdOperateur());
+            LOGGER.info("Operaeur updated : " + operateur.getNom());
+
+        }catch (Exception e){
+
+
+            System.out.println(e.getMessage());
+        }
+		
+	}
+	
+	@Test
+	public void TestRetrieveAllOperateur(){
+		
+		try {
+			operateurServiceImlp.retrieveAllOperateurs();
+            LOGGER.info("Retrieve All Operateur work");
+
+        }catch (Exception e){
+
+
+            System.out.println(e.getMessage());
+        }
+		
+	}
+
+	@Test
+	public void TestDeleteOperateur(){
+		
+		try {
+			operateurServiceImlp.deleteOperateur((long) 1);
+            LOGGER.info("Operaeur deleted");
+
+        }catch (Exception e){
+
+
+            System.out.println(e.getMessage());
+        }
+		
+	}
+
+	@Test
+	public void TestGetOperateur(){
+		
+		try {
+			Operateur operateur = operateurServiceImlp.retrieveOperateur((long) 1);
+            LOGGER.info("Operaeur : " + operateur.getNom());
 
         }catch (Exception e){
 
