c9190e54d65eaa00703b9b0b6603a4de858dfd98
Shayma REBHI <shayma.rebhi@esprit.tn>
2022-11-04 12:38:14 +0100

    clean code

--- src/main/java/com/esprit/examen/config/PerformanceAspect.java
+++ src/main/java/com/esprit/examen/config/PerformanceAspect.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Component
 @Aspect
