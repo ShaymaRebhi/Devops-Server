3780cca6e2b3169fb9fd0106745aef9b9d07b776
Shayma REBHI <shayma.rebhi@esprit.tn>
2022-11-06 00:03:33 +0100

    update

--- src/test/java/com/esprit/examen/services/StockServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/StockServiceImplTest.java
@@ -1,20 +1,18 @@
 package com.esprit.examen.services;
 
-import static org.junit.Assert.*;
+
 
 import java.text.ParseException;
 import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import com.esprit.examen.entities.Stock;
+import static org.junit.jupiter.api.Assertions.*;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 public class StockServiceImplTest {
     @Autowired
