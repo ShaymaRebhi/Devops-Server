e3739678d216cc630be653a89f73db30509e2788
OnsChebbi <48066211+OnsChebbi@users.noreply.github.com>
2022-11-08 21:03:15 +0100

    Update FournisseurServiceImplTest.java

--- src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
+++ src/test/java/com/esprit/examen/services/FournisseurServiceImplTest.java
@@ -1,4 +1,4 @@
-/*package com.esprit.examen.services;
+package com.esprit.examen.services;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -87,4 +87,4 @@ public class FournisseurServiceImplTest {
 
 
 	
-}*/
+}
